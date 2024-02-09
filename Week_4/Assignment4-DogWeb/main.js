733f0035dec3651a2b51c36129ce0a4e396ce4a1
Umugwaneza Cynthia <c.umugwanez@alustudent.com>
2024-02-09 15:26:23 +0200

    this is the dog javascript work

---
+++ Week_4/Assignment4-DogWeb/main.js
@@ -0,0 +1,13 @@
+const body= document.getElementById("body")
+const button= document.getElementById("button")
+button.addEventListener("click", ()=> {
+    // body.append("hello")
+    console.log("body");
+}
+)
+
+// fetch("https://random.dog/f58bc734-e3ba-44e7-8dbd-b7dfdd75936c.mp4"){
+//     .then(response)
+//     .then()
+//     .catch()
+// }
